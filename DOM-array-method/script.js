03351809cf9898e67d2b67c5f862f9f731ce9035
Stan Skrivanek <s.skrivanek@icloud.com>
2020-03-17 20:07:29 +0000

    add one user button funcionality

--- DOM-array-method/script.js
+++ DOM-array-method/script.js
@@ -22,11 +22,34 @@ async function getRandomUser() {
     money: Math.floor(Math.random() * 1000000)
   };
   addData(newUser);
-  // console.log(newUser);
+  console.log(newUser);
 }
 
 // Ass new obj to data array
 function addData(obj) {
   dataArr.push(obj);
+
+  updateDOM();
 }
-console.log("dataArr:", dataArr);
+
+// Update DOM
+function updateDOM(provideData = dataArr) {
+  // clear main div
+  main.innerHTML = "<h2><strong>Person</strong>Wealth</h2>";
+
+  provideData.forEach(item => {
+    const element = document.createElement("div");
+    element.classList.add("person");
+    element.innerHTML = `<strong>${item.fullName}</strong> ${formatMoney(
+      item.money
+    )}`;
+    main.appendChild(element);
+  });
+}
+// Format number as money - https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string
+function formatMoney(number) {
+  return "$ " + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,");
+}
+
+// event listeners
+addUserBtn.addEventListener("click", getRandomUser);
